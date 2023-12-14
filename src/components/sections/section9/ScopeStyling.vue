1570b1e59be27848df8fbf1442226b6e392b844d
AKSHAY SHIRSAT <arsofficial28@gmail.com>
2023-12-14 11:56:46 +0530

    commit

---
+++ src/components/sections/section9/ScopeStyling.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h3>hello techno</h3>
+    </header>
+</template>
+
+<style scoped>
+header{
+    width: 100%;
+    background-color: #b6a9e6;
+}
+
+</style>
---
+++ src/components/sections/section9/Slots.vue
@@ -0,0 +1,15 @@
+<!-- as a wrapper having some styling -->
+<template>
+    <div>
+      <p>Child Component Content:</p>
+      <!-- Use the default slot to display the injected content -->
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // No need for any additional script in this simple example
+  };
+  </script>
+  
